9e898b46def8493f1357ef7ead1bc5156310249a
victorval <vector386@gmail.com>
2020-05-26 14:23:21 -0500

    remove innecesary code from courses view and add thunk to setup

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -2,40 +2,18 @@ import React from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
-import { bindActionCreators } from 'redux';
+import { bindActionCreators } from "redux";
 
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: "",
-    },
-  };
-
-  handleChange = (event) => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  };
-
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <div>
+        {" "}
         <h2>Courses</h2>
-        <h3>Add course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
         {this.props.courses.map((course) => (
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+      </div>
     );
   }
 }
@@ -45,16 +23,16 @@ CoursesPage.propTypes = {
   actions: PropTypes.object.isRequired,
 };
 
-function mapStateToProps({courses}) {
+function mapStateToProps({ courses }) {
   return {
-    courses
+    courses,
   };
 }
 
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(courseActions, dispatch)
-  }
+    actions: bindActionCreators(courseActions, dispatch),
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
