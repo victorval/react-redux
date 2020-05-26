fd8c79da767be388d32cbac6ec4c8cfe9a2a0c87
victorval <vector386@gmail.com>
2020-05-26 16:24:55 -0500

    show the element in the actual view

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
 
 class CoursesPage extends React.Component {
   componentDidMount() {
-    this.props.actions.loadCourses().catch((err) => {
-      alert("Loading courses failed " + err);
-    });
+    if (this.props.courses === 0) {
+      this.props.actions.loadCourses().catch((err) => {
+        alert("Loading courses failed " + err);
+      });
+    }
+
+    if (this.props.authors === 0) {
+      this.props.actions.loadAuthors().catch((err) => {
+        alert("Loading authors failed " + err);
+      });
+    }
   }
+
   render() {
     return (
       <div>
@@ -23,19 +33,33 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
+  authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired,
 };
 
-function mapStateToProps({ courses }) {
+function mapStateToProps(state) {
   return {
-    courses,
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map((course) => {
+            return {
+              ...course,
+              authorName: state.authors.find((a) => a.id === course.authorId)
+                .name,
+            };
+          }),
+    authors: state.authors,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(courseActions, dispatch),
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+    },
   };
 }
 
