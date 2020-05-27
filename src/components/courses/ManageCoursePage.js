8f5e716e2608af2a88da501b53eb62b7a6738a5a
victorval <vector386@gmail.com>
2020-05-27 18:32:12 -0500

    implement redux flow component  again, update component

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,7 +1,7 @@
 // SECTION 1 Imports for container component
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { loadCourses } from "../../redux/actions/courseActions";
+import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import CourseForm from "./CourseForm";
@@ -13,27 +13,49 @@ function ManageCoursesPage({
   authors,
   loadAuthors,
   loadCourses,
+  saveCourse,
   ...props
 }) {
-
   const [course, setCourse] = useState({ ...props.course });
-  const [errors, setErrors] = useState({})
+  const [errors, setErrors] = useState({});
 
   useEffect(() => {
-    if (courses === 0) {
+    if (courses.length === 0) {
       loadCourses().catch((err) => {
         alert("Loading courses failed " + err);
       });
     }
 
-    if (authors === 0) {
+    if (authors.length === 0) {
       loadAuthors().catch((err) => {
         alert("Loading authors failed " + err);
       });
     }
   }, []);
 
-  return <CourseForm course={course} errors={errors} authors={authors} />;
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setCourse((prevCourse) => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value,
+    }));
+  }
+
+  function handleSave(event) {
+    event.preventDefault();
+    saveCourse(course);
+  }
+
+  return (
+    <CourseForm
+      course={course}
+      errors={errors}
+      authors={authors}
+      onChange={handleChange}
+      onSave={handleSave}
+    />
+  );
 }
 
 // SECTION 3 Prototypes declaration
@@ -43,6 +65,7 @@ ManageCoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
   loadAuthors: PropTypes.func.isRequired,
+  saveCourse: PropTypes.func.isRequired,
 };
 
 //SECTION 4 Redux mappings: state and actions to access in component
@@ -58,6 +81,7 @@ const mapDispatchToProps = {
   //this will bound call automatically for dispatch in component
   loadCourses,
   loadAuthors,
+  saveCourse,
 };
 
 // SECTION 5 connect
