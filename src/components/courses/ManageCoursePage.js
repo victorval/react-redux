f4919862adefda1f32ab856a4449829a14562415
victorval <vector386@gmail.com>
2020-05-27 10:55:51 -0500

    refactor of the components and the sections

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,24 +1,22 @@
-
 // SECTION 1 Imports for container component
 import React from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
-import { bindActionCreators } from "redux";
 
 // SECTION 2 Component declaration, pre-on-load and render
 class ManageCoursesPage extends React.Component {
   componentDidMount() {
-    const { courses, authors, actions } = this.props;
+    const { courses, authors, loadAuthors, loadCourses } = this.props;
     if (courses === 0) {
-      actions.loadCourses().catch((err) => {
+      loadCourses().catch((err) => {
         alert("Loading courses failed " + err);
       });
     }
 
     if (authors === 0) {
-      actions.loadAuthors().catch((err) => {
+      loadAuthors().catch((err) => {
         alert("Loading authors failed " + err);
       });
     }
@@ -38,7 +36,8 @@ class ManageCoursesPage extends React.Component {
 ManageCoursesPage.propTypes = {
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired
 };
 
 //SECTION 4 Redux mappings: state and actions to access in component
@@ -49,14 +48,11 @@ function mapStateToProps(state) {
   };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: {
-      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
-      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
-    },
-  };
-}
+const mapDispatchToProps = {
+  //this will bound call automatically for dispatch in component
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthors,
+};
 
 // SECTION 5 connect
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursesPage);
