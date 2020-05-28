887a00e610e8f2d7fab45979d3a955da4cb55b8a
victorval <vector386@gmail.com>
2020-05-28 10:10:10 -0500

    validation in client side before the err apicall

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -48,20 +48,36 @@ function ManageCoursesPage({
     }));
   }
 
+  function formIsValid() {
+    const { title, authorId, category } = course;
+    const errors = {};
+
+    if (!title) errors.title = "Title is required.";
+    if (!authorId) errors.author = "Author is required.";
+    if (!category) errors.category = "Category is required.";
+
+    setErrors(errors);
+
+    return Object.keys(errors).length === 0;
+  }
+
   function handleSave(event) {
     event.preventDefault();
+    if(!formIsValid()) return;
     setSaving(true);
-    saveCourse(course).then(() => {
-      toast.success("Course saved.")
-      history.push("/courses");
-    }).catch(error => {
-      setSaving(false);
-      setErrors({ onSave: error.message });
-    });
+    saveCourse(course)
+      .then(() => {
+        toast.success("Course saved.");
+        history.push("/courses");
+      })
+      .catch((error) => {
+        setSaving(false);
+        setErrors({ onSave: error.message });
+      });
   }
 
   return (authors.length === 0) | (courses.length === 0) ? (
-    <Spinner/>
+    <Spinner />
   ) : (
     <CourseForm
       course={course}
